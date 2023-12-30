347cc210a0345202a1a3a3c143b72de9bf39d2a8
Jazeye <jaseelp394@gmail.com>
2023-12-30 16:26:51 +0530

    new page & amount perc calc

--- main.js
+++ main.js
@@ -4,3 +4,18 @@ myCalulation = () => {
   let c = (a / b) * 100;
   document.getElementById("answer").innerHTML = c;
 };
+
+mySGPA = () => {
+  
+  let a = document.getElementById("sem").value;
+  let b = document.getElementById("totalsgpa").value;
+  let c = (e / b) * 100;
+  document.getElementById("answer").innerHTML = c;
+};
+
+myPercentage = () => {
+  let a = document.getElementById("score").value;
+  let b = document.getElementById("totalMark").value;
+  let c = (a /100 ) * b;
+  document.getElementById("answer").innerHTML = c;
+};
