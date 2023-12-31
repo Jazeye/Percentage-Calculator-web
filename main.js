e02da3c7fd465554c004c0123b9b6ec10ec24957
Jazeye <jaseelp394@gmail.com>
2023-12-31 15:13:29 +0530

    logic of sgpa added

--- main.js
+++ main.js
@@ -6,12 +6,33 @@ myCalulation = () => {
 };
 
 mySGPA = () => {
-  
-  let a = document.getElementById("sem").value;
-  let b = document.getElementById("totalsgpa").value;
-  let c = (e / b) * 100;
+  // Get user inputs
+  let a = parseFloat(document.getElementById("sem").value); // Convert to number
+  let b = parseFloat(document.getElementById("totalsgpa").value); // Convert to number
+
+  // Check if inputs are valid numbers
+  if (isNaN(a) || isNaN(b) || a < 1 || a > 6) {
+    document.getElementById("answer").innerHTML = "Invalid input";
+    return;
+  }
+
+  // Map the user input to corresponding values
+  const valueMapping = { 1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60 };
+  let mappedValue = valueMapping[a];
+
+  // Check if mapping exists
+  if (mappedValue === undefined) {
+    document.getElementById("answer").innerHTML = "Invalid input";
+    return;
+  }
+
+  // Perform calculations
+  let c = (b / mappedValue) * 100;
+
+  // Display the result
   document.getElementById("answer").innerHTML = c;
 };
+
 
 myPercentage = () => {
   let a = document.getElementById("score").value;
